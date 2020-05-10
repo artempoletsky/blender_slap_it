dcc355e6776448cd2eca4437ae5772a21ee5195a
Artem Poletsky <artempoletsky@gmail.com>
2020-05-10 13:15:46 +0500

    tris mesh progress

--- slap_it/slice_it.py
+++ slap_it/slice_it.py
@@ -37,12 +37,17 @@ def is_face_on_surface(face, bm):
     median = face.calc_center_median()
     min_distance = float('inf')
     for f in bm.faces:
-        d = abs(geometry.distance_point_to_plane(median, f.verts[0].co, f.normal))
+        d = abs(geometry.distance_point_to_plane(median, f.calc_center_median(), f.normal))
         min_distance = min(min_distance, d)
-    # print(min_distance)
-    return min_distance < 0.1
+    # if min_distance == 0:
+        # face.select_set(True)
+    # print(min_distance, min_distance < 0.1)
+    return min_distance == 0
 
-def sort_loops(face):
+def sort_tris_loops(face):
+    return [l.index for l in face.loops]
+
+def sort_quad_loops(face):
     if len(face.edges[0].link_faces) == 1 and len(face.edges[2].link_faces) == 1:
         loops = []
         loops.append(face.loops[3].index)
@@ -82,16 +87,23 @@ class SliceItOperator(bpy.types.Operator):
         bm = bmesh.new()
         bm.from_mesh(mesh)
         for f in bm.faces:
-            loops = sort_loops(f)
-            print(loops)
-            mesh.uv_layers.active.data[loops[0]].uv = [0, 0]
-            mesh.uv_layers.active.data[loops[1]].uv = [1, 0]
-            mesh.uv_layers.active.data[loops[2]].uv = [1, 1]
-            mesh.uv_layers.active.data[loops[3]].uv = [0, 1]
+            if len(f.verts) == 4:
+                loops = sort_quad_loops(f)
+                mesh.uv_layers.active.data[loops[0]].uv = [0, 0]
+                mesh.uv_layers.active.data[loops[1]].uv = [1, 0]
+                mesh.uv_layers.active.data[loops[2]].uv = [1, 1]
+                mesh.uv_layers.active.data[loops[3]].uv = [0, 1]
+            else:
+                loops = sort_tris_loops(f)
+                mesh.uv_layers.active.data[loops[0]].uv = [0, 0]
+                mesh.uv_layers.active.data[loops[1]].uv = [1, 0]
+                mesh.uv_layers.active.data[loops[2]].uv = [0.5, 1]
 
-    def hide_source_objects(self, context, target, brush):
-        select_only(context, target)
-        brush.select_set(True)
+
+    def hide_source_objects(self, context, objects):
+        select_only(context, objects[0])
+        for o in objects:
+            o.select_set(True)
         oops.hide_view_set(unselected = False)
 
     def create_slice_decal(self, context, target, brush):
@@ -113,13 +125,15 @@ class SliceItOperator(bpy.types.Operator):
         mops.select_all(action = 'DESELECT')
         oops.editmode_toggle()
         oops.duplicate()
-        slice_decal = context.object
         oops.convert(target = 'MESH')
+        target_dupli = context.object
+        oops.duplicate()
+        slice_decal = context.object
 
         brush_dupli.select_set(True)
         oops.join()
-        if self._stop_after_intersect:
-            self.hide_source_objects(context, target, brush)
+        if self._hide_source:
+            self.hide_source_objects(context, [target, brush, target_dupli])
         select_only(context, slice_decal)
         oops.editmode_toggle()
 
@@ -134,35 +148,54 @@ class SliceItOperator(bpy.types.Operator):
             mops.select_all(action = 'INVERT')
             mops.delete(type = 'VERT')
         else:
+            mops.select_mode(type='FACE', use_extend = False, use_expand = False)
             source_bm = bmesh.new()
-            source_bm.from_mesh(target.data)
+            source_bm.from_mesh(target_dupli.data)
             deleted_faces = [f for f in bm.faces if not is_face_on_surface(f, source_bm)]
             bmesh.ops.delete(bm, geom = deleted_faces, context = 'FACES')
             bmesh.update_edit_mesh(slice_decal.data, True)
             source_bm.free()
-            bm.free()
 
-        mops.select_all(action = 'SELECT')
-        mops.quads_convert_to_tris()
-        mops.tris_convert_to_quads(face_threshold = math.pi, shape_threshold = math.pi)
+        mops.select_mode(type='FACE', use_extend = False, use_expand = False)
+        if self._stop_before_triangulate:
+            raise
+        mops.select_all(action = 'DESELECT')
+        non_quads = [f for f in bm.faces if len(f.verts) != 4]
+        if len(non_quads):
+            for f in non_quads:
+                f.select_set(True)
+            mops.quads_convert_to_tris()
+            mops.tris_convert_to_quads(face_threshold = math.pi, shape_threshold = math.pi)
+            # non_quads = [f for f in bm.faces if len(f.verts) != 4]
+            # if len(non_quads):
+            #     self.report({'ERROR'}, "Can\'t create quads mesh decal!")
+            #     bm.free()
+            #     return None
+
+        bm.free()
         oops.editmode_toggle()
 
         collection = make_collection('Slice brushes', context.scene.collection)
         move_object_to_collection(context, brush, collection)
         context.view_layer.layer_collection.children['Slice brushes'].exclude = True
 
-
+        select_only(context, target_dupli)
+        oops.delete(use_global = False)
         select_only(context, slice_decal)
         # oops.editmode_toggle()
         return slice_decal
 
     def slice(self, context, target, brush):
         slice_decal = self.create_slice_decal(context, target, brush)
+        if not slice_decal:
+            return
         self.unwrap_slice_decal(context, slice_decal)
         self.assign_material(context, slice_decal)
 
     def execute(self, context):
+        self._hide_source = False
         self._stop_after_intersect = False
+        self._stop_before_triangulate = False
         selected_objects = context.selected_objects
         active_object = context.view_layer.objects.active
         selected_objects.remove(active_object)
